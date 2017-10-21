95838600ccf37fdf9ddb21727bfcb6c8becb341c
kafura <kafura.kafiri@gmail.com>
2017-10-21 21:42:30 +0330

    i am powerful

--- trans.py
+++ trans.py
@@ -12,7 +12,65 @@ dictionary = {
     'Search History': 'پیشینه جستجوها',
     'Clear Search History': 'حذف پیشینه',
 
-    'Read More&nbsp;&raquo;': '',
+    'Free Shipping': 'ارسال رایگان',
+    'for orders over': 'برای سفارشات بالای',
+    '20': '۲۰',
+    'EN': 'انگلیس',
+    'USD': 'آمریکا',
+    'Added to Cart': 'به سبد اضافه شد',
+    'Total': 'در مجموع',
+    'Discount': 'تخفیف',
+    'Together': 'باهم',
+    'Quantity': 'تعداد',
+    'Cart Total': 'مجموع سبد',
+    'Checkout': 'وارسی',
+    'Customers Also Bought': 'همچنین مشتریان خریدند',
+    '0.00': '۰.۰۰',
+    '0': '۰',
+    'Sign In': 'ورود',
+    'My Account': 'اکانت من',
+    'Reviews': 'نظرات',
+    'My Page': 'صفحه من',
+    'Rewards': 'جوایز',
+    'Messages': 'پیامها',
+    'Sign Out': 'خروج',
+    'Create an Account': 'ساخت اکانت',
+    'doloka.ir': 'doloka.ir',
+    '24/7': '۲۴/۷',
+    'Help': 'کمک',
+    'Brands': 'برندها',
+    'Conditions': 'شرایط',
+    'Herbs and Supplements': 'گیاهان و مکمل ها',
+    'Drug Interactions': 'تعاملات دارویی',
+    'HerbalGram': 'HerbalGram',
+    'Doloka Blog': '',
+    'Dr. Murray': '',
+    'All Links': 'تمام لینکها',
+    'Categories': 'دسته بندیها',
+    'New': 'تازه',
+    'Best Sellers': 'بهترین فروشندگان',
+    'Specials': 'ویژه ها',
+    'Trial Items': 'موارد محاکمه',
+    'Share': 'اشتراک',
+    'Copy and Share This Page': 'کپی و اشتراک گذاری صفحه',
+    'You are now signed in': 'هم اکنون وارد شدید',
+    'Copy and share this list with your Rewards code': 'کپی و اشتراک این لیست با کد پاداش شما',
+    'attached': 'الصاق',
+    'More about': 'اطلاعات بیشتر',
+    'Copy': 'کپی',
+    'Shorten URL': 'آدرس کوتاه',
+    'For Doloka customers &mdash; Share this page with others and save even more!': 'برای مشتریان دولوکا &mdash; اشتراک گذاری کنید و پول ذخیره کنید',
+    'or': 'یا',
+    'Read More': 'بیشتر بخوانید',
+    "World's Best Value!": 'بهترین ارزشهای جهان',
+
+    'For iHerb customers &mdash; Share this page with others and save even more!': 'برای مشتریان دولوکا &mdash; اشتراک گذاری کنید و پول ذخیره کنید',
+    'iHerb Blog': 'بلاگ دولوکا',
+    'Shipping Saver': 'صرفه جویی در ارسال',
+    'Add to Cart': 'اضافه به سبد',
+    '?': '؟',
+
+    'Read More&nbsp;&raquo;': 'بیشتر بخوانید',
     ''' &copy; Copyright 1997-2017 iHerb Inc. All rights reserved.
                   iHerb&reg; is a registered trademark of iHerb, Inc. Trusted Brands. Healthy Rewards.
                   and the iHerb.com Trusted Brands. Healthy Rewards. Logo are trademarks of iHerb,
@@ -30,7 +88,6 @@ dictionary = {
     'Supply Chains Act': 'زنجیرهای تامین',
     'Terms of Use': 'شرایط استفاده',
     'Privacy Policy': 'سیاست حفظ حریم خصوصی',
-    'Contact Us': 'تماس با ما',
     'Customer Service': 'خدمات مشتری',
     'Healthy Links': 'پیوندهای سالم',
     'My Doloka Page': 'دولوکای من',
@@ -61,9 +118,73 @@ dictionary = {
     'iHerb.com - Vitamins, Supplements &amp; Natural Health Products': 'دولوکا - ویتامین ها مکمل ها و طبیعیجات',
     'Live': 'زنده',
     'doloka': 'دولوکا',
-    'Check out what others are buying in real time!': 'محصولاتی که توسط هم میهنان تهیه میشود را به طور آنلاین چک کنید!'
+    'Check out what others are buying in real time!': 'محصولاتی که توسط هم میهنان تهیه میشود را به طور آنلاین چک کنید!',
 
-    ''
+
+    'iHerb.com': '',
+    'Email a Friend': 'ایمیل به دوستان',
+    'Saved': 'ذخیره شده',
+    'Your Name': 'نام شما',
+    'Your Friend&#39;s Email Address': 'آدرس ایمیل دوستتان',
+    'Invalid Email Address.': 'ایمیل نامعتبر',
+    'Personal Message': 'پیام شخصی',
+    'Optional (500 max characters)': 'اختیاری (ماکسیمم ۵۰۰ کاراکتر)',
+    'Send Email': 'ارسال ایمیل',
+    'Email a Friend!': 'ارسال ایمیل به دوست',
+    'Thank you!': 'از شما متشکریم!',
+    'Your email is being processed.': 'ایمیل شما مورد بررسی قرار گرفت',
+    'iHerb Customer': 'مشتری آیهرب',
+    'An iHerb Customer has completed a transaction, and has accepted the': 'یک مشتری آیهرب تراکنشی را تکمیل و قبول کرد',
+    'Terms & Conditions': 'شرایط و ظوابط',
+    'iHerb': 'آیهرب',
+
+    'Brands A-Z': 'برندها به ترتیب',
+    'By': 'توسط',
+    'You purchased this item on': 'شما خواهان این کالا شدید توسط',
+    'View this order': 'مشاهده این درخواست',
+    'Write a Review': 'یک نظر بنویسید',
+    'In Stock': 'در انبار',
+    'Switch to Metric units': 'تبدیل به واحدهای متریک',
+    'Sorry, this product is no longer in stock.': 'متاسفیم در انبار نیست',
+    'Related Products': 'محصولات مشابه',
+
+
+    '''Shipping Saver items cost less to ship, so we can pass the savings
+                  along to you! This means that, when you add a Shipping Saver item to your cart, your
+                  shipping cost will decrease.''': 'محصولات با پرچم صرفه جویی هزینه ارسال کمتری دارند. وقتی که چنین کالاهایی را به سبدتان بیفزایید کل هزینه ارسال کم میشود.',
+    'MSRP:': 'MSRP:',
+    'Our Price:': 'قیمت ما:',
+    'You Save:': 'شما صرفه جویی میکنید:',
+    '''Sorry,
+                            this product is no longer in stock.''': 'متاسفیم ****',
+    'Call': 'تماس',
+    'Add to Wish List': 'اضافه به لیست علاقه مندیها',
+    'Create a New List:': 'ایجاد لیست جدید',
+    'List Name Required': 'اسم لیست را وارد کنید',
+    'Create List': 'ساخت لیست',
+    'Earn': 'به دست آورید',
+    'Loyalty Credit': 'اعتبار وفاداری',
+    'Loyalty Program': 'برنامه وفاداری',
+    '- You earn loyalty credit for every order you place at iHerb!': '- شما هر گونه سفارشی را که در iHerb قرار می دهید، اعتبار وفاداری کسب می کنید!',
+    '-&nbsp;&nbsp;Loyalty credit is equal to 5% of the value of your order after discounts and credits applied, excluding shipping charges.': '- & nbsp؛ و nbsp؛ اعتبار وفاداری برابر 5٪ ارزش سفارش شما است پس از تخفیف و اعتبار اعمال شده، به استثنای هزینه حمل و نقل.',
+    '- Your loyalty credit will be applied towards your next order as soon as your current order has shipped.': '- اعتبار وفاداری شما به منظور سفارش بعدی شما به محض ارسال سفارش فعلی شما اعمال خواهد شد.',
+    '- Your loyalty credits are valid for up to 90 days from the date of your last purchase.': '- اعتبار وفاداری شما تا 90 روز از تاریخ آخرین خرید شما معتبر است.',
+    'Contain:': 'حاوی',
+
+    'Product Overview': 'بررسی اجمالی محصول',
+    'Visit Manufacturer&#39;s Website': 'بازدید از سایت تامین کننده',
+    'Amount Per Serving': 'مقدار صرف در هر وعده',
+    '%DV': '٪ DV',
+    """*Daily Value (DV) not established.<br/> †This product uses Gnosis
+                                     S.p.A.'s L-5-Methyltetrahydrofolic acid, Glucosamine Salt<br/> ††This product
+                                     uses Albion&rsquo;s TRAACS Calcium Bisglycinate Chelate, Chromium Nicotinate
+                                     Glycinate Chelate, Copper Bisglycinate Chelate, Manganese Bisglycinate Chelate,
+                                     Magnesium Bisglycinate Chelate, Molybdenum Glycinate Chelate, and Zinc
+                                     Bisglycinate Chelate. TRAACS is a registered trademark of Albion International,
+                                     Inc. <br/> †††This product uses Albion&rsquo;s Bororganic Boron Glycinate
+                                     Complex. Bororganic is a trademark of Albion International, Inc.<br/>""": '',
+
+    'Search from over 35,000 products': 'از بین ۳۵۰۰۰ هزار محصول به جستوجو بپردازید',
 }
 
 
@@ -72,12 +193,50 @@ def simplify(phrase):
     translator = str.maketrans('', '', string.punctuation)
     return phrase.translate(translator).lower().strip()
 
+def digify(num):
+    digits = {
+        '0': '۰',
+        '1': '۱',
+        '2': '۲',
+        '3': '۳',
+        '4': '۴',
+
+        '5': '۵',
+        '6': '۶',
+        '7': '۷',
+        '8': '۸',
+        '9': '۹',
+
+        '.': '.',
+    }
+    num = str(num)
+    _num = []
+    for digit in num:
+        _num.append(digits[digit])
+    return ''.join(_num)
 
 def trans(phrase):
-    phrase = simplify(phrase)
-    if phrase in dictionary:
-        return dictionary[phrase]
-    return phrase
+    CSI = "\x1B[%sm"
+    W = '\033[0m'  # white (normal)
+    R = '\033[31m'  # red
+    G = '\033[32m'  # green
+    O = '\033[33m'  # orange
+    B = '\033[34m'  # blue
+    P = '\033[35m'  # purple
+    if isinstance(phrase, str):
+        _phrase = phrase
+        phrase = simplify(phrase)
+        if phrase in dictionary:
+            return dictionary[phrase]
+        print(CSI % '5;30;41' + "'" + _phrase + "': ''," + CSI % '0')
+        return phrase
+    import numbers
+    if isinstance(phrase, numbers.Real):
+        print(CSI % '6;30;42' + "'" + 'an int i saw %f' % phrase + "': ''," + CSI % '0')
+        return digify(phrase)
+    else:
+        print(CSI % '35' + 'wtf what was that: %s' % phrase + CSI % '0')
+        return phrase
 
 
 def update():
